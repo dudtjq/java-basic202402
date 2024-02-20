3371111b8b68b1258484ae6d96a954a9582b8d01
dudtjq <dudtjq0415@naver.com>
2024-02-20 11:43:22 +0900

    # initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("안녕하세요");
+        System.out.println("sout 치고 바로 엔터~~");
+        System.out.println("shift + f10을 하면 코드를 바로 돌릴 수 있다");
+
+    }
+}
