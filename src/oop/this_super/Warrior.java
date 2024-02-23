4b6be0f34bef4f1bb5f1d8e1c14455d67165e7eb
dudtjq <dudtjq0415@naver.com>
2024-02-23 17:30:07 +0900

    # Java oop(this)

---
+++ src/oop/this_super/Warrior.java
@@ -0,0 +1,15 @@
+package oop.this_super;
+
+public class Warrior extends Player {
+
+    int rage;
+
+
+    @Override
+    void characterInfo(){
+        super.characterInfo();
+        System.out.println("# 분노 : " + rage);
+    }
+
+
+}
