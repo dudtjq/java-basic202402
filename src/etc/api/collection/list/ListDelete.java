cf14a815e298a3d6580599e9c62636ca4abba774
dudtjq <dudtjq0415@naver.com>
2024-03-04 18:18:06 +0900

    # Java oop(무한 루프를 사용한 문제풀이로 수정)

--- src/etc/api/collection/list/ListDelete.java
+++ src/etc/api/collection/list/ListDelete.java
@@ -15,8 +15,14 @@ public class ListDelete {
         Collections.addAll(kakao, "무지", "네오", "어피치", "라이언", "제이지", "춘식이");
         System.out.println("현재 저장된 친구들 : " + kakao);
 
-        System.out.print("삭제할 친구를 입력하세요 : ");
-        String name = scan.nextLine();
+        while (true){
+
+            if(kakao.isEmpty()){
+                break;
+            }
+
+            System.out.print("삭제할 친구를 입력하세요 : ");
+            String name = scan.nextLine();
 
         /*
          - 입력받은 이름을 리스트 내에서 삭제하세요.
@@ -24,14 +30,20 @@ public class ListDelete {
          - 없으면 없는 이름이라고 출력해 주세요.
          */
 
-        if(!kakao.contains(name)){
-            System.out.println("이름이 포함되어 있지 않습니다.");
-        }else {
-            kakao.remove(name);
-            System.out.println("삭제할 정보 : " + kakao);
+            if(!kakao.contains(name)){
+                System.out.println("이름이 포함되어 있지 않습니다.");
+                continue;
+            }else {
+                kakao.remove(name);
+                System.out.println("삭제할 정보 : " + kakao);
+            }
+
         }
 
+
         scan.close();
+
+
 
 
     }
