3f1f2c8f528861c4934281416818229881df22f6
dudtjq <dudtjq0415@naver.com>
2024-05-10 18:18:02 +0900

    자바 스트림 퀴즈

---
+++ src/etc/stream/quiz/Main.java
@@ -0,0 +1,97 @@
+package etc.stream.quiz;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Trader raoul = new Trader("Raoul", "Cambridge");
+        Trader mario = new Trader("Mario", "Milan");
+        Trader alan = new Trader("Alan", "Cambridge");
+        Trader brian = new Trader("Brian", "Cambridge");
+
+        List<Transaction> transactions = List.of(
+                new Transaction(brian, 2021, 300),
+                new Transaction(raoul, 2022, 1000),
+                new Transaction(raoul, 2021, 400),
+                new Transaction(mario, 2021, 710),
+                new Transaction(mario, 2022, 700),
+                new Transaction(alan, 2022, 950)
+        );
+
+        // 연습 1: 2021년에 발생한 모든 거래를 찾아 거래액 오름차 정렬(작은 값에서 큰 값).
+        transactions.stream()
+                .filter(transaction -> transaction.getYear() == 2021)
+                .sorted(Comparator.comparing(Transaction::getValue))
+                .toList()
+                .forEach(System.out::println);
+
+        System.out.println("===============================================================================");
+
+        // 연습 2: 거래자가 근무하는 모든 도시 이름을 중복 없이 나열하시오.
+        transactions.stream()
+                .map(transaction -> transaction.getTrader().getCity())
+                .distinct()
+                .toList()
+                .forEach(System.out::println);
+
+        System.out.println("===============================================================================");
+
+        // 연습 3: Cambridge에 근무하는 모든 거래자를 찾아 거래자 리스트로 이름순으로 오름차 정렬.
+        transactions.stream()
+                .map(Transaction::getTrader)
+                .filter(transaction -> transaction.getCity().equals("Cambridge"))
+                .sorted(Comparator.comparing(Trader::getName))
+                .toList()
+                .forEach(System.out::println);
+        System.out.println("===============================================================================");
+
+        // 연습 4: 모든 거래자의 이름을 리스트에 모아서 알파벳순으로  오름차 정렬하여 반환
+        List<String> nameList = transactions.stream()
+                .map(transaction -> transaction.getTrader().getName())
+                .distinct()
+                // sorted 는 문자나 숫자는 Comparator 를 사용 하지 않아도 무방함
+                .toList();
+        Collections.sort(nameList); // 이미 알고 있는 정렬 방식
+        System.out.println("nameList = " + nameList);
+        
+        System.out.println("===============================================================================");
+
+        // 연습 5: Milan에 거주하는 거래자가 한명이라도 있는지 여부 확인?
+        boolean flag1 = transactions.stream()
+                .anyMatch(transaction -> transaction.getTrader().getCity() == "Milan");
+
+        System.out.println("flag1 = " + flag1);
+
+        System.out.println("===============================================================================");
+
+        // 연습 6: Cambridge에 사는 거래자의 모든 거래액의 총합 출력.
+        int sum = 0;
+        transactions.stream()
+                .filter(transaction -> transaction.getTrader().getCity() == "Cambridge")
+                .forEach((transaction) -> ( sum += transaction.getValue());
+
+        System.out.println("===============================================================================");
+
+        // 연습 7: 모든 거래에서 최고 거래액은 얼마인가?
+        Transaction transaction = transactions.stream()
+                .max(Comparator.comparing(Transaction::getValue)).get();
+
+        System.out.println("transaction = " + transaction);
+
+        System.out.println("===============================================================================");
+
+        // 연습 8. 가장 작은 거래액을 가진 거래정보 탐색
+        Transaction transaction1 = transactions.stream()
+                .min(Comparator.comparing(Transaction::getValue)).get();
+
+        System.out.println("transaction1 = " + transaction1);
+
+
+    }
+
+}
